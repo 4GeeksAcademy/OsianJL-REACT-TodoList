e5aa742184d3eba91865cf4861c52e1a464a5416
Osián <149536063+OsianJL@users.noreply.github.com>
2024-02-12 09:38:35 +0000

    todo-list almost done

--- src/js/component/task.jsx
+++ src/js/component/task.jsx
@@ -17,7 +17,7 @@ export const Task = () => {
         }
     }
 
-    
+
 
     return (
         <div className='container d-flex  fs-1'><span className='todos'>todos</span>
@@ -25,12 +25,17 @@ export const Task = () => {
                 <input type="text" className="input-task" placeholder="what needs to be done?" onKeyDown={createList} onChange={(e) => setTask(e.target.value)} value={task} />
 
                 <div>
-                    <ul style={{listStyle:"none"}}>
-                        {taskList.map((item, index) => <li className='list-unit' key={index}>{item}<a className='close-btn' href='#' >x</a></li>)}
-                        <div className='bottomCard'>4 items left</div>
+                    <ul style={{ listStyle: "none" }}>
+                        {taskList.map((item, index) =>
+
+                        (<li className='list-unit' key={index}>
+                            {item}<a className='close-btn' href='#' onClick={() => setTaskList(taskList.filter((t, currentIndex) => index !== currentIndex))}>x</a>
+                        </li>))}
+
+                        <div className='bottomCard'>{taskList.length} items left</div>
                         <div className='bottomCard1'></div>
                         <div className='bottomCard2'></div>
-                     
+
                     </ul>
                 </div>
 
@@ -46,7 +51,7 @@ export const Task = () => {
 
     )
 
-    }
+}
 
 export default Task
 
